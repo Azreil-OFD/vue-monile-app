88ef5842a2cd1d322a0470906bd784f8e76ea446
Azreil-OFD <fantom2413@gmail.com>
2023-11-20 17:36:10 +0500

    fix base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vue-monile-app/",
   plugins: [vue()],
   server: {
     host: "192.168.91.128",
