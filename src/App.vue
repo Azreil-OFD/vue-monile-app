52c1bde111c600c0836a2899b43cc60fc6c3d641
Azreil-OFD <fantom2413@gmail.com>
2023-11-21 03:34:07 +0500

    fix App and add Assessments pass

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Home from './components/Home.vue'
 import Rasp from './components/Rasp.vue'
-
+import Assessments from './components/Assessments.vue';
 import { ref } from "vue"
 
 
@@ -10,13 +10,49 @@ const mode = ref("События")
 </script>
 
 <template>
-  <button @click="mode = 'События'">События</button>
-  <button @click="mode = 'Расписание'">Расписание</button>
-  <button @click="mode = 'Оценки'">Оценки</button>
+  <h1>{{ mode }}</h1>
+  <div class="buttons">
+    <button class="blob-btn " @click="mode = 'События'" :class="{ 'active': mode == 'События' }">
+      События
+      <span class="blob-btn__inner">
+        <span class="blob-btn__blobs">
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+        </span>
+      </span>
+    </button>
+    <button class="blob-btn " @click="mode = 'Расписание'" :class="{ 'active': mode == 'Расписание' }">
+      Расписание
+      <span class="blob-btn__inner">
+        <span class="blob-btn__blobs">
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+        </span>
+      </span>
+    </button>
+
+    <button class="blob-btn " @click="mode = 'Оценки'" :class="{ 'active': mode == 'Оценки' }">
+      Оценки
+      <span class="blob-btn__inner">
+        <span class="blob-btn__blobs">
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+        </span>
+      </span>
+    </button>
+  </div>
+
 
 
   <Home v-if="mode == 'События'"></Home>
   <Rasp v-if="mode == 'Расписание'"></Rasp>
+  <Assessments v-if="mode == 'Оценки'"></Assessments>
 </template>
 
 <style scoped>
@@ -25,5 +61,143 @@ const mode = ref("События")
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
+}
+
+.tab {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+
+.buttons {
+  text-align: center;
+  border-radius: 30px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.blob-btn {
+  z-index: 1;
+  position: relative;
+  padding: 5px 10px;
+  text-align: center;
+  text-transform: uppercase;
+  color: #6c6c6c;
+  font-size: 13px;
+  background-color: transparent;
+  outline: none;
+  border: none;
+  transition: color 0.5s;
+  cursor: pointer;
+  border-radius: 30px;
+}
+
+.blob-btn:before {
+  content: "";
+  z-index: 1;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  border: 2px solid #6c6c6c;
+  border-radius: 30px;
+}
+
+.blob-btn:after {
+  content: "";
+  z-index: -2;
+  position: absolute;
+  left: 3px;
+  top: 3px;
+  width: 100%;
+  height: 100%;
+  transition: all 0.3s 0.2s;
+  border-radius: 30px;
+}
+
+.blob-btn:hover {
+  color: #FFFFFF;
+  border-radius: 30px;
+}
+
+.blob-btn:hover:after {
+  transition: all 0.3s;
+  left: 0;
+  top: 0;
+  border-radius: 30px;
+}
+
+.blob-btn__inner {
+  z-index: -1;
+  overflow: hidden;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 30px;
+  background: #ffffff;
+}
+
+.blob-btn__blobs {
+  position: relative;
+  display: block;
+  height: 100%;
+  filter: url("#goo");
+}
+
+.blob-btn__blob {
+  position: absolute;
+  top: 2px;
+  width: 25%;
+  height: 100%;
+  background: #6c6c6c;
+  border-radius: 100%;
+  transform: translate3d(0, 150%, 0) scale(1.7);
+  transition: transform 0.45s;
+}
+
+@supports (filter: url("#goo")) {
+  .blob-btn__blob {
+    transform: translate3d(0, 150%, 0) scale(1.4);
+  }
+}
+
+
+.blob-btn__blob:nth-child(1) {
+  left: 0%;
+  transition-delay: 0s;
+}
+
+.blob-btn__blob:nth-child(2) {
+  left: 30%;
+  transition-delay: 0.08s;
+}
+
+.active {
+  text-decoration: underline;
+}
+
+.blob-btn__blob:nth-child(3) {
+  left: 60%;
+  transition-delay: 0.16s;
+}
+
+.blob-btn__blob:nth-child(4) {
+  left: 90%;
+  transition-delay: 0.24s;
+}
+
+.blob-btn:hover .blob-btn__blob {
+  transform: translateZ(0) scale(1.7);
+}
+
+@supports (filter: url("#goo")) {
+  .blob-btn:hover .blob-btn__blob {
+    transform: translateZ(0) scale(1.4);
+  }
 }
 </style>
---
+++ src/components/Assessments.vue
@@ -0,0 +1,12 @@
+<template>
+    <h3 class="element">В разработке...</h3>
+</template>
+
+<style scoped>
+.element {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
